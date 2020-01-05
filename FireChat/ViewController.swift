116396c340103aff6e9fe07879b8adaf350c5886
Dawid Jaskulski <dawid.jaskulski98@gmail.com>
2020-01-05 15:21:21 +0100

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Dawid Jaskulski on 05/01/2020.
+//  Copyright © 2020 Dawid Jaskulski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
